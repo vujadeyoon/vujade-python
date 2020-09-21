341f5ccbf9034cf39e9f4adb07715a9bc0616fcf
Sungjun Ethan Yoon <sjyoon1671@gmail.com>
2020-09-21 13:50:51 +0900

    200921a

---
+++ vujade/vujade_yaml.py
@@ -0,0 +1,24 @@
+"""
+Dveloper: vujadeyoon
+E-mail: sjyoon1671@gmail.com
+Github: https://github.com/vujadeyoon/vujade
+Date: Sep. 21, 2020.
+
+Title: vujade_videocv.py
+Version: 0.1
+Description: A module for yaml.
+"""
+
+
+import yaml
+
+class vujade_yaml():
+    def __init__(self, _path_filename):
+        super(vujade_yaml, self).__init__()
+        self.path_filename = _path_filename
+
+    def read(self):
+        with open(self.path_filename) as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+
+        return data
